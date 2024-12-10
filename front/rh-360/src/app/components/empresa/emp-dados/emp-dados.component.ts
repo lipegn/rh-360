9937deeee2b6e401625a2465efd4cd1fe6e50c8b
Felipe Gonçalves <lipegn2@gmail.com>
2024-12-10 17:11:51 -0300

    tela formulário

--- front/rh-360/src/app/components/empresa/emp-dados/emp-dados.component.ts
+++ front/rh-360/src/app/components/empresa/emp-dados/emp-dados.component.ts
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './emp-dados.component.scss'
 })
 export class EmpDadosComponent {
+  empresa: Partial<{
+    nome: string
+    , cnpj: string
+    , adm: string
+    , cpf: string
+    , email: string
+    , telefone: string
+    , cep: string
+    , endereco: string
+  }> = {}
 
+  constructor() {
+    this.empresa = {
+      cnpj: '11.394.715/0001-92'
+      , adm: 'Meu RH 360*'
+      , cpf: '111.222.111.33'
+      , email: 'emailbombo@gmail.com'
+      , telefone: '(33) 9999-9999'
+      , cep: '33333-333'
+      , endereco: 'Avenida anhaia melo, 3333'
+      , nome: 'Meu RH 360*'
+    }
+  }
 }
